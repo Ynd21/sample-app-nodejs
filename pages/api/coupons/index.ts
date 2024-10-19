0afb55cc5b7dd663b7053ac6c4a1b8561096954c
Josh <Joshie787@Gmail.com>
2024-10-19 00:51:59 -0500

    up up

--- pages/api/coupons/index.ts
+++ pages/api/coupons/index.ts
@@ -6,9 +6,14 @@ export default async function coupons(req: NextApiRequest, res: NextApiResponse)
         const { accessToken, storeHash } = await getSession(req);
         const bigcommerce = bigcommerceClient(accessToken, storeHash);
         const { page, limit, sort, direction } = req.query;
-        const params = new URLSearchParams({ page, limit, ...(sort && {sort, direction}) }).toString();
 
-        const response = await bigcommerce.get(`/coupons?${params}`);
+        const params = new URLSearchParams();
+        if (typeof page === 'string') params.append('page', page);
+        if (typeof limit === 'string') params.append('limit', limit);
+        if (typeof sort === 'string') params.append('sort', sort);
+        if (typeof direction === 'string') params.append('direction', direction);
+
+        const response = await bigcommerce.get(`/coupons?${params.toString()}`);
         res.status(200).json(response);
     } catch (error) {
         const { message, response } = error;
